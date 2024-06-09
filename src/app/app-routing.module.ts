37b2066bc536b79e9a7991ab241feb7e1283e8c7
David-alzate <alzatedavid0126@gmail.com>
2024-06-09 11:24:43 -0500

    Creacion de los componenetes de la aplicacion

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { RegistroClienteComponent } from './registro-cliente/registro-cliente.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/registro-cliente', pathMatch: 'full' },
+  { path: 'registro-cliente', component: RegistroClienteComponent },
+  { path: 'iniciar-sesion', component: LoginComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegistroClienteComponent } from './registro-cliente/registro-cliente.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegistroClienteComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
